4d38f4d3f38efd6d402f1cf48c9aa39d2232b499
Gabriel Baptista <software.architecture@outlook.com>
2026-04-21 20:16:40 -0300

    Small changes

--- ch06/GenericHost/Program.cs
+++ ch06/GenericHost/Program.cs
@@ -1,31 +1,19 @@
 ﻿using Microsoft.Extensions.Hosting;
-using System.IO;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using GenericHost;
 
-namespace GenericHost
-{
-    public class Program
-    {
-        public static void Main()
-        {
-            CreateHostBuilder().Build().Run();
-            Console.WriteLine("Host has terminated. Press any key to finish the App.");
-            Console.ReadKey();
-        }
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder() =>
-            Host.CreateDefaultBuilder()
-            .ConfigureServices((hostContext, services) =>
-            {
-                services.AddHostedService<HostedService>();
-                services.AddHostedService<MonitoringService>();
-            })
-            .ConfigureLogging((hostContext, configLogging) =>
-            {
-                configLogging.AddConsole();
-            });
-    }
-}
+builder.Services.AddHostedService<HostedService>();
+builder.Services.AddHostedService<MonitoringService>();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+using IHost host = builder.Build();
+host.Run();
+
+Console.WriteLine("Host has terminated. Press any key to finish the app.");
+Console.ReadKey();
